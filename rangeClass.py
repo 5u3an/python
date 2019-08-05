8e183254bca7ef9f6ea31a9e28e6568423cf69bc
Suman Khadka <suman@suman.localdomain>
2019-08-05 14:55:18 +0545

    First commit

---
+++ rangeClass.py
@@ -0,0 +1,13 @@
+# limit =20
+# for anything in range(limit,2, -3):
+#     print(anything)
+
+
+userInput = input("Enter anything: ")
+
+ipIsLower = userInput.islower()
+print(userInput)
+# userInput = userInput.lower()
+# if userInput == 'a':
+#     print("Ok!")
+
